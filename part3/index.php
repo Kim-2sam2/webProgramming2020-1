20d183e7cd22ece73e41f61545c08dfb22889fe7
khwoo8026 <khwoo8026@gmail.com>
2020-05-26 04:40:49 +0900

    11week complete

--- part3/index.php
+++ part3/index.php
@@ -5,16 +5,54 @@
 <title>PHP 프로그래밍 입문</title>
 <link rel="stylesheet" type="text/css" href="./css/common.css">
 <link rel="stylesheet" type="text/css" href="./css/main.css">
+<link href="css/Icomoon/style.css" rel="stylesheet" type="text/css" />
+<link href="css/animated-header.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="scripts/jquery-2.0.2.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.10.4.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.isotope.min.js"></script>
+<script type="text/javascript" src="scripts/animated-header.js"></script>
 </head>
-<body> 
-	<header>
-    	<?php include "header.php";?>
-    </header>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <div id="header-inner">
+            <div id="header-inner-logo">
+                <div id="header-inner-logo-icon"><span class="iconb" data-icon="&#xe150;"></span></div>
+                <div id="header-inner-logo-text"><span>PHP</span>안녕하세요</div>
+            </div>
+            <div id="header-inner-nav">
+                <p data-button="1">Header</p>
+                <p data-button="2">main</p>
+                <p data-button="3">footer</p>
+            </div>
+        </div>
+	</div>
+</div>
+
+<div class="tab" data-section="1">
+    <img src=" assets/1.jpg" />
+    <div class="tab-headline">
+		<header>
+			<?php include "header.php";?>
+		</header>
+	</div>
+</div>
+<div class="tab" data-section="2">
+    <img src="assets/2.jpg" />
+    <div class="tab-headline">
 	<section>
 	    <?php include "main.php";?>
 	</section> 
+	</div>
+</div>
+<div class="tab" data-section="3">
+    <img src="assets/3.jpg" />
+    <div class="tab-headline">
 	<footer>
     	<?php include "footer.php";?>
     </footer>
+	</div>
+</div>
+
 </body>
 </html>
