164c25b565879f2b825358d3e2effb04649e28b2
kim-2sam2 <khwoo8026@naver.com>
2020-05-26 21:14:12 +0900

    start_project

---
+++ webProject/header.php
@@ -0,0 +1,18 @@
+<div id="top">
+    <h3>
+        <a href="index.php">TITLE</a>
+    </h3>
+    <ul id="top_info">
+        <li>info1</li>
+        <li>info2</li>
+    </ul>
+</div>
+
+<div id="menu_bar">
+    <ul>
+        <li>HOME</li>
+        <li>My List</li>
+        <li>Board</li>
+        <li>Ranking</li>
+    </ul>
+</div>
---
+++ webProject/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>20160705 김현우</title>
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+
+</head>
+<body>
+    <header>
+        <?php include "header.php"?>
+    </header>
+    <section>
+        <?php include "main.php"?>
+    </section>
+    <footer>
+        <?php include "footer.php"?>
+    </footer>
+
+</body>
+</html>
