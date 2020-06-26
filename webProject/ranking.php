ec49b0df0b8c23e4defbe8875f67d78b7d518bc6
khwoo8026 <khwoo8026@gmail.com>
2020-06-26 14:28:49 +0900

    ranking

---
+++ webProject/ranking.php
@@ -0,0 +1,46 @@
+<?php
+include 'func.php';
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>20160705</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/aside.css" />
+    <script type="text/javascript" src="scripts/common.js"></script>
+    <script type="text/javascript" src="scripts/mylist.js"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <?php include "header.php"; ?>
+        </header>
+
+        <section class="middle">
+
+
+
+
+
+
+
+
+            <?php include "aside.php"; ?>
+        </section>
+
+
+        <footer>
+            <?php include "footer.php"; ?>
+
+        </footer>
+    </div>
+</body>
+
+</html>
