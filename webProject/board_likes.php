c795287508441055a321fd680f633fa55fe681b7
khwoo8026 <khwoo8026@gmail.com>
2020-06-26 14:23:36 +0900

    view bug fix

--- webProject/board_likes.php
+++ webProject/board_likes.php
@@ -5,13 +5,15 @@ session_start();
 $con = connectDB();
 $list_index = $_GET["num"];
 
-$sql = "select likes from todoList_20160705 where num = $list_index";
+$sql = "select likes, view from todoList_20160705 where num = $list_index";
 $result = mysqli_query($con, $sql);
 $row = mysqli_fetch_array($result);
 $likes = $row["likes"];
+$view = $row["view"];
 $new_likes = $likes + 1;
+$new_view = $view - 1;
 
-$sql = "update todoList_20160705 set likes=$new_likes where num=$list_index";
+$sql = "update todoList_20160705 set likes=$new_likes, view=$new_view where num=$list_index";
 mysqli_query($con, $sql);
 mysqli_close($con);
 
